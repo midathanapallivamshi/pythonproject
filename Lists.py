1e2eb908431069954e7b17c3701c31ec8f3d5084
vamshi <midathanapallivamshi@gmail.com>
2018-09-26 22:23:56 +0530

    add file

---
+++ Lists.py
@@ -0,0 +1,15 @@
+#to create a list
+fruits=["apple","banana","cherry"]
+print(fruits)
+#to print the element using index value
+fruits=["apple","banana","cherry"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+#to change the specfic element on the lsit
+fruits=["apple","banana","cherry"]
+fruits[1]="mango"
+print(fruits)
+#add
+print(fruits.append(fruits))
+
